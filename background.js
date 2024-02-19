f0cb475f515d2e9c920c9c335af7832ed6b069b1
Frenkieli <49364993+Frenkieli@users.noreply.github.com>
2024-02-20 00:56:05 +0800

    feat: update code

--- background.js
+++ background.js
@@ -1,8 +1,9 @@
-// background.js
+// 安裝腳本時設定 default 值
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.local.set({ AutoRemove: false }); // 将默认值设置为 false
+  chrome.storage.local.set({ AutoRemove: false });
 });
 
+// 當點擊擴展圖標時，這個監聽器將會被觸發
 chrome.action.onClicked.addListener((tab) => {
   chrome.storage.local.get("AutoRemove", (data) => {
     const isAutoRemoveEnabled = !data.AutoRemove;
@@ -15,23 +16,24 @@ chrome.action.onClicked.addListener((tab) => {
         : "icons/logo-disabled.png";
       chrome.action.setIcon({
         path: {
-          16: iconPath,
+          128: iconPath,
         },
       });
 
-      // 向当前标签页的内容脚本发送消息
+      // 這個消息將被發送到 content-script.js ，但是在非 URL 匹配的情況下會導致 Uncaught (in promise) Error: Could not establish connection. Receiving end does not exist.
+      // 因為需要獲取 tab query 去判斷是否是正確的 URL 會需要額外權限，所以這邊就放任它發生錯誤
       chrome.tabs.sendMessage(tab.id, { AutoRemove: isAutoRemoveEnabled });
     });
   });
 });
 
-// 在 background.js 中
+// 當 content-script.js 發送消息時，這個監聽器將會被觸發
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.query === "getAutoRemoveStatus") {
     chrome.storage.local.get("AutoRemove", (data) => {
       sendResponse({ AutoRemove: data.AutoRemove });
     });
-    return true; // 保持消息通道打开，以便异步发送响应
+    return true;
   }
 });
 
@@ -40,7 +42,11 @@ function updateIcon() {
     const iconPath = data.AutoRemove
       ? "icons/logo-enabled.png"
       : "icons/logo-disabled.png";
-    chrome.action.setIcon({ path: iconPath });
+    chrome.action.setIcon({
+      path: {
+        128: iconPath,
+      },
+    });
   });
 }
 
--- content-script.js
+++ content-script.js
@@ -1,5 +1,6 @@
 if (new URLSearchParams(window.location.search).get("id") === "47") {
   const title = document.title;
+  const iconUrl = document.querySelector('link[rel="shortcut icon"]').href;
 
   setTimeout(() => {
     chrome.runtime.sendMessage({ query: "getAutoRemoveStatus" }, (response) => {
@@ -10,23 +11,37 @@ if (new URLSearchParams(window.location.search).get("id") === "47") {
   }, 0);
 
   chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.AutoRemove === false) {
-      const customStyle = document.getElementById("custom-style");
-      if (customStyle) {
-        customStyle.remove();
-      }
-
-      document.title = title;
-    } else {
+    if (message.AutoRemove) {
       removeAll();
+    } else {
+      addAll();
     }
   });
 
+  function addAll() {
+    const customStyle = document.getElementById("custom-style");
+    if (customStyle) {
+      customStyle.remove();
+    }
+
+    document.title = title;
+
+    if (document.querySelector('link[rel="shortcut icon"]')) {
+      document.querySelector('link[rel="shortcut icon"]').href = iconUrl;
+    }
+  }
+
   function removeAll() {
-    document.title = "New Title";
+    document.title = chrome.i18n.getMessage("newTitle");
+
+    if (document.querySelector('link[rel="shortcut icon"]')) {
+      document.querySelector('link[rel="shortcut icon"]').href =
+        chrome.runtime.getURL("icons/logo-enabled.png");
+    }
+
     const css = document.createElement("style");
 
-    css.id = "custom-style"; // 给<style>元素设置一个唯一的id
+    css.id = "custom-style";
     css.textContent = `
           #game_frame {
             width: 395px !important;
@@ -37,8 +52,8 @@ if (new URLSearchParams(window.location.search).get("id") === "47") {
             justify-content: center;
           }
 
-          #nav, #footer {
-            display: none;
+          #nav, #footer, .footer_mobileNav.mobile {
+            display: none !important;
           }
         `;
 
